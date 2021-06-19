a2d4c08bb027bdef79f2463d43c173eca7bbfbe4
חיים לייבמן <hnl770@gmail.com>
2021-06-20 00:27:50 +0300

    Updates

---
+++ precache-manifest.0f88360d756a73c10b1610981e97d08d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa1b69fc7965eca0f0d5723f5ec05ff",
+    "url": "/open-weather-client/index.html"
+  },
+  {
+    "revision": "d51bba17456f5173de73",
+    "url": "/open-weather-client/static/css/main.40154240.chunk.css"
+  },
+  {
+    "revision": "b9abe90092b217b4f2d4",
+    "url": "/open-weather-client/static/js/2.c2f3baa4.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/open-weather-client/static/js/2.c2f3baa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51bba17456f5173de73",
+    "url": "/open-weather-client/static/js/main.142fb68a.chunk.js"
+  },
+  {
+    "revision": "e5c2787a7664fcd6bce0",
+    "url": "/open-weather-client/static/js/runtime-main.94712e19.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d6a8d74c3fe3e453bc807affa7a37a4.js"
+  "/open-weather-client/precache-manifest.0f88360d756a73c10b1610981e97d08d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/open-weather-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
